c0b1f9cbe880089df57df80977db434282561d98
ebukalinux <ebukanwafor2020@gmail.com>
2022-09-08 15:45:09 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,6 +18,6 @@ int main(void)
 	printf(“Size of a char: %ld byte(s)\n”, sizeof (c));
 	printf(“Size of an int: %ld byte(s)\n”, sizeof(i));
 	printf(“Size of a long int: %ld byte(s)\n”, sizeof(lli));
-	printf(Size of a flot: %ld byte(s)\n”, sizeof (f));
+	printf(Size of a float: %ld byte(s)\n”, sizeof (f));
 	return (0);
 }
