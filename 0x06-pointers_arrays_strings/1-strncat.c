2509cd7ad61cf0e0fcf0b921778625480f33c398
ebukalinux <ebukanwafor2020@gmail.com>
2022-09-21 19:16:31 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-#include “main.h”
+#include "main.h"
 /**
  *_strncat - concatenate two stringd but add inputted number of bytes
  *@dest: string to be appended upon
@@ -9,13 +9,14 @@
 
 char *_strncat(char*dest, char *src, int n)
 {
+
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src{index];
+		dest[dest_len++] = src[index];
 
 	return (dest);
 
